c78a252b8c34d28c924f2439d58f131ed61c582f
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-04-10 12:08:21 +0200

    Remove luv from lazydev after nvim 0ee5a4d

--- editors/nvim/after/ftplugin/lua.lua
+++ editors/nvim/after/ftplugin/lua.lua
@@ -4,8 +4,4 @@ vim.b.lspfmt = "stylua3p_ls"
 
 -- Add neovim lua modules to path
 MiniDeps.add("folke/lazydev.nvim")
-require("lazydev").setup({
-	library = {
-		{ path = "${3rd}/luv/library", words = { "vim%.uv", "vim%.loop" } },
-	},
-})
+require("lazydev").setup()
