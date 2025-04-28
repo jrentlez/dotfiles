4c3f7e759bbf1a5f71315fb4b0d6fe5997b558f2
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-04-28 12:19:23 +0200

    Make prompt shell agnostic

--- terminal/nushell/prompt/src/main.rs
+++ terminal/nushell/prompt/src/main.rs
@@ -52,30 +52,34 @@ fn venv() -> Option<String> {
     Some(color::GREEN.to_string() + " (" + &venv_prompt + ")")
 }
 
-fn status() -> Option<String> {
-    let status = var_lossy("LAST_EXIT_CODE")?;
-    if status != "0" {
-        Some(color::RED.to_string() + " " + &status)
+fn status(last_status: u8) -> Option<String> {
+    if last_status != 0 {
+        Some(color::RED.to_string() + " " + &last_status.to_string())
     } else {
         None
     }
 }
 
 fn main() {
-    let job_count = env::args()
-        .nth(1)
+    let mut args = env::args();
+    let _ = args.next();
+    let job_count = args
+        .next()
         .and_then(|jc| jc.parse::<usize>().ok())
         .unwrap_or_default();
+    let last_status = args
+        .next()
+        .and_then(|ls| ls.parse::<u8>().ok())
+        .unwrap_or_default();
+
     let userhost = userhost().unwrap_or_default();
     let (dir, repo) = directory();
-    let gstat = match repo {
-        Some(repo) => git(&repo),
-        None => "".to_string(),
-    };
+    let gstat = repo.map(|repo| git(&repo)).unwrap_or_default();
     let pyvenv = venv().unwrap_or_default();
-    let stat = status().unwrap_or_default();
+    let stat = status(last_status).unwrap_or_default();
     println!(
-        "\n╭{userhost}{dir}{gstat}{pyvenv}\n{}╰{}{stat} ",
+        "\n{}╭{userhost}{dir}{gstat}{pyvenv}\n{}╰{}{stat} ",
+        color::GREEN,
         color::GREEN,
         if job_count > 0 {
             color::BLUE.to_string() + " ✦"
