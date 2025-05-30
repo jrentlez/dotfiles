051abb1a5600d2a776629adf3c88bfd6752538c1
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-05-30 06:10:25 +0200

    Replace git-conflict with custom plugin

--- editors/nvim/plugin/conflict.lua
+++ editors/nvim/plugin/conflict.lua
@@ -1,44 +1,25 @@
 ---@module "mini.deps"
 
-local add, now = MiniDeps.add, MiniDeps.now
+local add, later = MiniDeps.add, MiniDeps.later
 
-now(function()
-	add({ source = "akinsho/git-conflict.nvim", checkout = "v2.1.0", monitor = "v2.1.0" })
+later(function()
+	add({ source = "https://gitlab.com/TungstnBallon/conflict.nvim" })
 
-	require("git-conflict").setup({
-		debug = false,
-		default_mappings = false, -- disable buffer local mapping created by this plugin
-		default_commands = false, -- disable commands created by this plugin
-		disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
-		list_opener = "copen", -- command or function to open the conflicts list
-		highlights = {
-			current = "DiffText",
-			incoming = "DiffAdd",
-			ancestor = "DiffChange",
-		},
-	})
+	vim.keymap.set("n", "gC", function()
+		require("conflict").jump_to_next_conflict(nil, nil, true)
+	end)
 
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "GitConflictDetected",
-		---@param args {buf: number}
-		callback = function(args)
-			vim.keymap.set("n", "grh", function()
-				vim.ui.select({ "ours", "theirs", "both", "base", "none" }, {
-					prompt = "Select a variant to keep",
-					---@param item ConflictSide
-					format_item = function(item)
-						return "Keep " .. item
-					end,
-				}, function(item, _)
-					if item then
-						require("git-conflict").choose(item)
-					end
-				end)
-			end, { buffer = args.buf, desc = "Resolve conflict" })
-
-			vim.keymap.set("n", "gC", function()
-				require("git-conflict").find_next("ours")
-			end, { buffer = args.buf, desc = "Goto next conflict" })
-		end,
-	})
+	vim.keymap.set("n", "grn", function()
+		vim.ui.select({ "current", "base", "incoming", "none", "both" }, {
+			prompt = "Select a variant to keep",
+			format_item = function(item)
+				return "Keep " .. item
+			end,
+		}, function(item)
+			if item then
+				require("conflict").resolve_conflict_at(nil, nil, item)
+			end
+		end)
+		require("conflict").jump_to_next_conflict(nil, nil, true)
+	end)
 end)
