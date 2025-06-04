85516637f416fe78d151f838c8172d51d8b6d4bf
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-06-04 09:31:27 +0200

    Use the plugin's own mappings

--- editors/nvim/plugin/conflict.lua
+++ editors/nvim/plugin/conflict.lua
@@ -5,21 +5,12 @@ local add, later = MiniDeps.add, MiniDeps.later
 later(function()
 	add({ source = "TungstnBallon/conflict.nvim" })
 
-	vim.keymap.set("n", "gC", function()
-		require("conflict").jump_to_next_conflict(nil, nil, true)
-	end)
+	vim.keymap.set("n", "gC", "<Plug>ConflictJumpToNext", { desc = "Jump to the next conflict in the current buffer" })
 
-	vim.keymap.set("n", "grh", function()
-		vim.ui.select({ "current", "base", "incoming", "none", "both" }, {
-			prompt = "Select a variant to keep",
-			format_item = function(item)
-				return "Keep " .. item
-			end,
-		}, function(item)
-			if item then
-				require("conflict").resolve_conflict_at(nil, nil, item)
-			end
-		end)
-		require("conflict").jump_to_next_conflict(nil, nil, true)
-	end)
+	vim.keymap.set(
+		"n",
+		"grh",
+		"<Plug>ConflictResolveAroundCursor",
+		{ desc = "Resolve the conflict around the current cursor position" }
+	)
 end)
