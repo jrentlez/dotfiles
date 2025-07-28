b798531aceb1627d98621f2301ba70284b9bfbdc
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-07-28 22:40:35 +0200

    Add `adwaita` and `default_terminal` colorschemes

---
+++ editors/nvim/colors/default_terminal.lua
@@ -0,0 +1,9 @@
+vim.cmd.highlight("clear")
+
+if vim.g.syntax_on == 1 then
+	vim.cmd.syntax("reset")
+end
+
+vim.g.colors_name = "default_terminal"
+
+vim.api.nvim_set_hl(0, "Normal", { force = true })
