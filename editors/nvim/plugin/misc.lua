4fa7369cefb0f6f796f8d4f6d61abb7ed35f4796
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-08-04 14:49:01 +0200

    Remove conflict.nvim, it doesn't work :(

--- editors/nvim/plugin/misc.lua
+++ editors/nvim/plugin/misc.lua
@@ -1,12 +1,38 @@
--- Jayvee
 vim.pack.add({ "https://github.com/jvalue/jayvee.nvim.git" })
 
-vim.schedule(function()
-	-- Conflict
-	vim.pack.add({ "https://github.com/TungstnBallon/conflict.nvim.git" })
-	vim.keymap.set("n", "gC", "<Plug>ConflictJumpToNext", { desc = "Go to next conflict" })
-	vim.keymap.set("n", "<leader>r", "<Plug>ConflictResolveAroundCursor", { desc = "Resolve conflict around cursor" })
+---@param lines string[]
+---@return boolean
+local function go_to_marker_in(lines)
+	local marker_row ---@type integer?
+	for idx, line in ipairs(lines) do
+		if line == "=======" then
+			marker_row = idx + 1
+			break
+		end
+	end
+	if marker_row then
+		vim.api.nvim_win_set_cursor(0, { marker_row, 0 })
+		return true
+	else
+		return false
+	end
+end
 
-	-- Extui
+local function goto_next_conflict_marker()
+	local row = unpack(vim.api.nvim_win_get_cursor(0))
+	local lines = vim.api.nvim_buf_get_lines(0, row, -1, false)
+
+	if go_to_marker_in(lines) then
+		return
+	end
+	lines = vim.api.nvim_buf_get_lines(0, 0, row, false)
+	if go_to_marker_in(lines) then
+		return
+	end
+end
+
+vim.schedule(function()
 	require("vim._extui").enable({})
+
+	vim.keymap.set("n", "gC", goto_next_conflict_marker, { desc = "Go to next conflict marker" })
 end)
