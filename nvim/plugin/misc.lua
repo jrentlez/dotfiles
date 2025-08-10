f83816e7f99ae7bad5c7ef058ecbe4e043a6ae6b
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-08-10 13:56:17 +0200

    Add message if no conflict marker was found

--- nvim/plugin/misc.lua
+++ nvim/plugin/misc.lua
@@ -21,20 +21,26 @@ local function go_to_marker_in(lines)
 	end
 end -- }}}
 
-local function goto_next_conflict_marker()
-	local row = unpack(vim.api.nvim_win_get_cursor(0))
-	local lines = vim.api.nvim_buf_get_lines(0, row, -1, false)
+---@param window? integer
+---@return boolean
+local function goto_next_conflict_marker(window)
+	window = window or 0
+	local buffer = vim.api.nvim_win_get_buf(window)
+	local row = vim.api.nvim_win_get_cursor(window)[1]
+	local lines_from_cursor_to_end = vim.api.nvim_buf_get_lines(buffer, row, -1, false)
 
-	if go_to_marker_in(lines) then
-		return
+	if go_to_marker_in(lines_from_cursor_to_end) then
+		return true
 	end
-	lines = vim.api.nvim_buf_get_lines(0, 0, row, false)
-	if go_to_marker_in(lines) then
-		return
-	end
+	local lines_from_start_to_cursor = vim.api.nvim_buf_get_lines(buffer, 0, row, false)
+	return go_to_marker_in(lines_from_start_to_cursor)
 end -- }}}
 
 vim.schedule(function()
 	require("vim._extui").enable({})
-	vim.keymap.set("n", "gC", goto_next_conflict_marker, { desc = "Go to next conflict marker" })
+	vim.keymap.set("n", "gC", function()
+		if not goto_next_conflict_marker() then
+			vim.notify("No conflict marker found", vim.log.levels.INFO)
+		end
+	end, { desc = "Go to next conflict marker" })
 end)
