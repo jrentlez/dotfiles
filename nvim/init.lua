cd23178860e42d1c1e97f7c9d8d6c9fbfeab9b80
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-08-14 12:22:00 +0200

    Disable extui, move keybind out of plugin, dissolve misc.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -59,7 +59,23 @@ vim.keymap.set({ "i", "s" }, "<C-h>", function()
 end)
 vim.keymap.set({ "i", "s" }, "<C-l>", function()
 	vim.snippet.jump(1)
-end) -- }}}
+end)
+
+vim.keymap.set("n", "gC", function()
+	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	local row ---@type integer?
+	for line_idx_one_based, line in ipairs(lines) do
+		if line == "=======" then
+			row = line_idx_one_based
+			break
+		end
+	end
+	if row then
+		vim.api.nvim_win_set_cursor(0, { row, 0 })
+	else
+		vim.notify("No conflict marker found", vim.log.levels.INFO)
+	end
+end, { desc = "Go to first conflict marker" }) -- }}}
 -- {{{ Autocommands
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("hl_on_yank", { clear = true }),
---
+++ nvim/plugin/jayvee.lua
@@ -0,0 +1,1 @@
+vim.pack.add({ "https://github.com/jvalue/jayvee.nvim.git" }, { load = true })
