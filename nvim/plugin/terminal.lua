c796b4469a6e108e9f97dd22142e316cf40d1a8b
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-09-02 09:16:16 +0200

    follow example from |lua-plugin-keymaps|

--- nvim/plugin/terminal.lua
+++ nvim/plugin/terminal.lua
@@ -1,4 +1,4 @@
 vim.schedule(function()
 	vim.pack.add({ "https://github.com/TungstnBallon/split-term.nvim" })
-	vim.keymap.set({ "n", "t" }, "<C-Space>", "<Plug>SplitTermToggle")
+	vim.keymap.set({ "n", "t" }, "<C-Space>", "<Plug>(SplitTermToggle)")
 end)
