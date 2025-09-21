b9d25c005f19435fde4db7aed67849bdb6f7b329
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-09-21 08:47:11 +0200

    fix: make sure callbacks don't recieve autocmd parameters

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -32,12 +32,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 				buffer = event.buf,
 				group = lsp_augroup,
-				callback = vim.lsp.buf.document_highlight,
+				callback = function()
+					vim.lsp.buf.document_highlight()
+				end,
 			})
 			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 				buffer = event.buf,
 				group = lsp_augroup,
-				callback = vim.lsp.buf.clear_references,
+				callback = function()
+					vim.lsp.buf.clear_references()
+				end,
 			})
 		end
 	end,
