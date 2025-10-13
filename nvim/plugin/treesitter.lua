f10182396c8fcc145fa5fea55967cff18a066355
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-10-13 18:08:04 +0200

    refactor: use commands instead of lua api and formatting

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("PackChanged", {
 		local kind = args.data.kind --[[@as "install" | "update" | "delete"]]
 		local spec = args.data.spec --[[@as vim.pack.SpecResolved]]
 		if spec.name == "nvim-treesitter" and kind ~= "delete" then
-			require("nvim-treesitter").update({ summary = true })
+			vim.cmd("TSUpdate")
 		end
 	end,
 })
@@ -35,17 +35,7 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.schedule(function()
 	local required_parsers = vim.tbl_filter(function(parser)
 		return not vim.list_contains(require("nvim-treesitter").get_installed(), parser)
-	end, {
-		"c",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"query",
-		"vim",
-		"vimdoc",
-		"comment",
-		"diff",
-	})
+	end, { "c", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc", "comment", "diff" })
 	if not vim.tbl_isempty(required_parsers) then
 		require("nvim-treesitter").install(required_parsers, { summary = true })
 	end
