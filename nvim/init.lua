5b586215788addcdd0115080d33e50fe45edb1e4
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-10-13 18:04:46 +0200

    feat(options): set colorcolumn with textwidth and highlight current line number

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,8 @@ vim.g.clipboard = "osc52"
 
 vim.o.breakindent = true
 vim.o.completeopt = "menuone,fuzzy,noinsert,popup"
+vim.o.colorcolumn = "+1"
+vim.o.cursorlineopt = "number"
 vim.o.cursorline = true
 vim.o.exrc = true
 vim.o.ignorecase = true
